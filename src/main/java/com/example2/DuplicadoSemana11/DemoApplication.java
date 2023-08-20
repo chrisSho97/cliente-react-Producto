205603112a190b56541ba1ee2706f7fb8849f335
chrisSho97 <christianpancich_97@hotmail.com>
2023-08-20 12:42:53 -0500

    ejecutando

--- src/main/java/com/example2/DuplicadoSemana11/DemoApplication.java
+++ src/main/java/com/example2/DuplicadoSemana11/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DuplicadoSemana11Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DuplicadoSemana11Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/main/java/com/example2/DuplicadoSemana11/VentaDetalleRepository.java
+++ src/main/java/com/example2/DuplicadoSemana11/VentaDetalleRepository.java
@@ -1,9 +1,9 @@
 package com.example2.DuplicadoSemana11;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+    import org.springframework.data.repository.CrudRepository;
+    import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "ventadetalles", path = "ventadetalles")
-public interface VentaDetalleRepository extends CrudRepository<VentaDetalle, Long>{
-    
-}
+    @RepositoryRestResource(collectionResourceRel = "ventadetalles", path = "ventadetalles")
+    public interface VentaDetalleRepository extends CrudRepository<VentaDetalle, Long>{
+        
+    }
